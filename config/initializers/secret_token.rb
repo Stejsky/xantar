12ed65a444692b2cfbda04477b04354dc0a20e6b
Stejsky <jakub@stejsky.com>
2014-02-18 20:41:58 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Xantar::Application.config.secret_key_base = '17062f71ff9ef2cbf8dfc0be4c1459dbc6be3d5dc5511b686897cc88f7cebfc0c0b7327ec1bd0fe331100ef9c750fcbc2b993cd8c73ca86ad1346ae1d9b619ab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Xantar::Application.config.session_store :cookie_store, key: '_xantar_session'
